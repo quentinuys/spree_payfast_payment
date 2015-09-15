e0155b7a3dc3f4ba57a8bef90bdaa383e840264e
Quentin Langeveldt <quentinuys@gmail.com>
2015-09-15 02:48:35 +0200

    gg

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  get '/payfast_success' => 'payfast_return#success'
-  get '/payfast_failure' => 'payfast_return#failure'
-  get '/payfast_cancel'  => 'payfast_return#cancel'
+  post '/payfast_success' => 'payfast_return#success'
+  post '/payfast_failure' => 'payfast_return#failure'
+  post '/payfast_cancel'  => 'payfast_return#cancel'
 end
